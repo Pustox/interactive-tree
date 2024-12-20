f335224a8234093f0463f0bb4a2f2a747d497a42
Oksana <pustoxsun@gmail.com>
2024-12-20 08:57:35 +0100

    vol +

--- script.js
+++ script.js
@@ -19,16 +19,16 @@ navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
       const volume = dataArray.reduce((a, b) => a + b) / dataArray.length;
 
       // Управление прозрачностью ветвей ёлки
-      if (volume > 5) treeLevels[0].style.opacity = '1';
-      if (volume > 10) treeLevels[1].style.opacity = '1';
-      if (volume > 20) treeLevels[2].style.opacity = '1';
-      if (volume > 30) star.style.opacity = '1';
+      if (volume > 20) treeLevels[0].style.opacity = '1';
+      if (volume > 30) treeLevels[1].style.opacity = '1';
+      if (volume > 40) treeLevels[2].style.opacity = '1';
+      if (volume > 50) star.style.opacity = '1';
 
       // Если тише, то гасим
-      if (volume < 30) star.style.opacity = '0.2';
-      if (volume < 20) treeLevels[2].style.opacity = '0.2';
-      if (volume < 10) treeLevels[1].style.opacity = '0.2';
-      if (volume < 5) treeLevels[0].style.opacity = '0.2';
+      if (volume < 50) star.style.opacity = '0.2';
+      if (volume < 40) treeLevels[2].style.opacity = '0.2';
+      if (volume < 30) treeLevels[1].style.opacity = '0.2';
+      if (volume < 20) treeLevels[0].style.opacity = '0.2';
     }
 
     requestAnimationFrame(animateTree);
